968676c085c8d6cf8c24d2ad38e866e89ea18a77
todaychen <todaychen@tencent.com>
2021-01-20 17:10:27 +0800

    加入环境变量

--- functions/get_suite_access_token/index.php
+++ functions/get_suite_access_token/index.php
@@ -1,13 +1,11 @@
 <?php
 include "./vendor/autoload.php";
-const SUITE_ID = "bec9873c53dc11eb93b2be80f293bd92";
-const SUITE_SECRET = "19D2IhpCk9jKgNZO9F32zbfEflcatAQ7LN1suBsi";
-const REDIS_HOST = "192.168.2.49";
-const REDIS_PASSWORD = 'edRDs41a9Xhf0ghu';
 function main($event, $context) {
+    $environment = json_decode($context->environment);
+
     $redis = new Redis();
-    $redis->connect(REDIS_HOST);
-    $redis->auth(REDIS_PASSWORD);
+    $redis->connect($environment->REDIS_HOST);
+    $redis->auth($environment->REDIS_PASSWORD);
     if ($suite_access_token = $redis->get('suite_access_token')) {
         return $suite_access_token;
     }
@@ -15,8 +13,8 @@ function main($event, $context) {
     $suite_ticket = $redis->get('suite_ticket');
     $options = ['form_params' => [
         'grant_type' => 'client_credentials',
-        'suite_id' => SUITE_ID,
-        'suite_secret' => SUITE_SECRET,
+        'suite_id' => $environment->SUITE_ID,
+        'suite_secret' => $environment->SUITE_SECRET,
         'suite_ticket' => $suite_ticket
     ]];
 
--- functions/suite_callback/index.js
+++ functions/suite_callback/index.js
@@ -1,40 +1,40 @@
-const cloudbase = require('@cloudbase/node-sdk')
+const cloudBase = require('@cloudbase/node-sdk')
 const redis = require('redis')
 const mysql = require('serverless-mysql')({
     config: {
-        host: "192.168.2.77",
-        port: 3306,
-        database: "forest",
-        user: 'forest_web',
-        password: 'forest_web'
+        host: process.env.DEFAULT_MYSQL_HOST,
+        port: process.env.DEFAULT_MYSQL_PORT,
+        database: process.env.DEFAULT_MYSQL_DATABASE,
+        user: process.env.DEFAULT_MYSQL_USER,
+        password: process.env.DEFAULT_MYSQL_PASSWORD
     }
 })
 
 
-const app = cloudbase.init({})
-var db = app.database();
+const app = cloudBase.init({})
+const db = app.database();
 
 exports.main = async (event, context) => {
-    if (event.httpMethod == "POST") {
+    if (event.httpMethod === "POST") {
         const body = JSON.parse(event.body)
-        db.collection("suite_callback_logs").add(body)
+        await db.collection("suite_callback_logs").add(body)
 
         console.log(body.action)
-        if (body.action == "service/suite_ticket") {
-            redis_client = await redis.createClient({
-                host: "192.168.2.49",
-                port: 6379,
-                password: 'edRDs41a9Xhf0ghu'
+        if (body.action === "service/suite_ticket") {
+            const redis_client = await redis.createClient({
+                host: process.env.DEFAULT_REDIS_HOST,
+                port: process.env.DEFAULT_REDIS_PORT,
+                password: process.env.DEFAULT_REDIS_PASSWORD
             })
             await redis_client.setex("suite_ticket", 1200, body.attributes.suite_ticket)
             redis_client.quit()
-        } else if (body.action == "service/create_auth") {
+        } else if (body.action === "service/create_auth") {
 
-            call_res = await app.callFunction({
+            let call_res = await app.callFunction({
                 name: "get_suite_access_token"
             })
 
-            suite_access_token = call_res.result
+            let suite_access_token = call_res.result
             call_res = await app.callFunction({
                 name: "get_corp_info",
                 data: {
@@ -45,7 +45,7 @@ exports.main = async (event, context) => {
             let addSqlParams = [call_res.result.data.company_id, call_res.result.data.permanent_code]
             await mysql.query("INSERT INTO companies(id, lexiang_uuid, permanent_code, created_at) VALUES('', ?, ?, now())", addSqlParams)
             mysql.quit()
-        } else if (body.action == "service/cancel_auth") {
+        } else if (body.action === "service/cancel_auth") {
             let addSqlParams = [body.attributes.company_id]
             await mysql.query("DELETE FROM companies WHERE lexiang_uuid = ?", addSqlParams)
             mysql.quit()

717b1a19b50c91eb8dee850d75b97fd4f9f6b872
todaychen <todaychen@tencent.com>
2021-02-25 16:45:15 +0800

    add vue

--- vue/src/pages/auth-callback/index.vue
+++ vue/src/pages/auth-callback/index.vue
@@ -15,17 +15,20 @@ export default {
     const auth = app.auth();
 
     const loginState = await auth.getLoginState();
+    console.log(loginState);
     const { code } = this.$route.query;
     // 1. 建议登录前检查当前是否已经登录
     if (!loginState) {
       // 2. 请求开发者自有服务接口获取ticket
-      const ticket = await axios.get('/login_user', {
+      const response = await axios.get('https://todayapi.lexiangla.net/login_user', {
         params: {
           code,
         },
       });
+      console.log(response);
       // 3. 登录 CloudBase
-      await auth.customAuthProvider().signIn(ticket);
+      const result = await auth.customAuthProvider().signIn(response.data.data.ticket);
+      console.log(result);
     }
   },
 }

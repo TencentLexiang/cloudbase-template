521b00fd59331a4efc425c12c9f70a15e0917dca
todaychen <todaychen@tencent.com>
2021-03-03 16:57:58 +0800

    post course

--- functions/post_course/index.js
+++ functions/post_course/index.js
@@ -1,11 +1,50 @@
 const cloudBase = require('@cloudbase/node-sdk');
+const axios = require('axios');
 
 const app = cloudBase.init({});
-const auth = app.auth();
 
 exports.main = async(event, context) => {
-    const user = auth.currentUser;
-    console.log(user);
+    const company_id = event.company_id;
+    const staff_id = event.staff_id;
+    const attributes = event.attributes;
+    const corp_token = await app.callFunction({
+        name: "get_corp_token",
+        data: {
+            "company_id": company_id
+        }
+    }).then(function(response) {
+        return response.result;
+    });
 
-    return "ok";
+    return await axios.post(process.env.LX_API_URL + "v1/courses",
+        {
+            "data": {
+                "type": "course",
+                "attributes": {
+                    "title": attributes.title,
+                    "content": attributes.content,
+                    "media_type": 5,
+                    "target_users": attributes.target_users,
+                    "video_link": attributes.video_link
+                },
+                "relationships": {
+                    "category": {
+                        "data": {
+                            "type": "category",
+                            "id": attributes.category_id
+                        }
+                    }
+                }
+            }
+        }, {
+            headers: {
+                "Content-Type": "application/vnd.api+json",
+                "StaffID": staff_id,
+                "Authorization": "Bearer " + corp_token
+            }
+        }).then(function(response) {
+            console.log(response.data);
+            return response.data.data;
+        }
+    );
 }

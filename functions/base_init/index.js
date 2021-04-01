94bc2212377030e10ed1bce45f8eb697c568a85b
todaychen <todaychen@tencent.com>
2021-04-01 15:04:02 +0800

    bugfix

--- functions/base_init/index.js
+++ functions/base_init/index.js
@@ -1,23 +1,22 @@
-const { init } = require('@cloudbase/manager-node');
-const CloudBase = require('@cloudbase/manager-node')
-
-
-const app = cloudBase.init({
+const manager = require('@cloudbase/manager-node').init({
     envId: process.env.ENV_ID
+});
+const app = require('@cloudbase/node-sdk').init({
+    env: process.env.ENV_ID
 });
 const db = app.database();
 
 exports.main = async (event, context) => {
     // 只执行一次，无论成功与否
     const init_config = await db.collection("lx_suites").doc("init_config").get().then(function(res) {
-        return res.data[0].done;
+        return res.data[0];
     });
-    if (init_config.done) {
+    if (init_config && init_config.done) {
         return;
     }
-    await db.collection("lx_suites").doc("init_config").update({done: 1});
+    await db.collection("lx_suites").doc("init_config").set({done: 1});
 
-    const api_domain = await app.commonService().call({
+    const api_domain = await manager.commonService().call({
         Action: 'DescribeCloudBaseGWService',
         Param: {
             ServiceId: process.env.ENV_ID
@@ -27,12 +26,15 @@ exports.main = async (event, context) => {
         return res.DefaultDomain;
     });
 
-    const page_doamin = await app.commonService().call({
+    const page_doamin = await manager.commonService().call({
         Action: 'DescribeStaticStore',
         Param: {
             EnvId: process.env.ENV_ID
         }
     })
+    .then((res) => {
+        return res.Data[0].CdnDomain;
+    });
 
     let init_attributes = {
         "redirect_domain": page_doamin,
